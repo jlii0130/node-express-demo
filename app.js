5e5844c5427b0df53d1fb6b84bdd373a36c8dd1b
JJ <jlii0130@student.monash.edu>
2020-04-25 20:56:52 +1000

    Update app.js

--- app.js
+++ app.js
@@ -30,11 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname, { dotfiles: 'allow'}));
 
-app.listen(80,() => {
-    console.log('http server running on port 80')
-})
 
 // ---- OAuth2.0 ---- //
 // Helmet: enhance the security
