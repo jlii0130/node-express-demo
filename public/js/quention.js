376c4a3836e04db4dd1ee805da27402f86ea401f
JJ <jlii0130@student.monash.edu>
2020-04-25 19:53:16 +1000

    add file

--- public/js/quention.js
+++ public/js/quention.js
@@ -1,12 +1,29 @@
-function finishchoose() {
-    var selectarea = document.getElementById("selectionarea");
-    var valarea = selectarea.option[selectarea.selectedIndex].value
+
+var url = 'http://ttie-env.eba-9cjewynp.ap-southeast-2.elasticbeanstalk.com'
+// });
+
+function choose() {
+
+    //var selectarea = document.getElementById("selectionarea");
+    //var valarea = selectarea.option[selectarea.selectedIndex].value
+
+    var valarea = $( "#selectionarea option:selected" ).text();
+    var valfruit = $( "#selectionfruit option:selected" ).text();
     
-    var selectfruit = document.getElementById("selectionfruit");
-    var valfruit = selectfruit.option[selectfruit.selectedIndex].value
+    //var selectfruit = document.getElementById("selectionfruit");
+    //var valfruit = selectfruit.option[selectfruit.selectedIndex].value
 
-    if (val == "Caenarvon") {
+    console.log(valarea)
+    console.log(valfruit)
 
-    }
-
-}
+    $.ajax({
+        url: this.url + '/api/get/' + valarea + '/' + valfruit,
+        
+        success:function(data) {
+            console.log(data)
+            let div = $("#qwert")
+            let para = '<p>' + data[0].Preventive_Methods + data[0].Control_Methodds+'</p>'
+            div.append(para)
+        }
+    })
+} 
