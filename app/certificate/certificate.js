c41738afa1988a46fc71b5ff776836e956277617
JJ <jlii0130@student.monash.edu>
2020-04-25 20:24:17 +1000

    Update certificate.js

--- app/certificate/certificate.js
+++ app/certificate/certificate.js
@@ -4,10 +4,12 @@ const app = express();
 
 
 app.get("/TsDhB-4GL6UBREEQkAgr7ajKL6CVMYoVQE0tF16v-w0", (req, res) => {
+    res.charset = 'utf-8'
     res.send('TsDhB-4GL6UBREEQkAgr7ajKL6CVMYoVQE0tF16v-w0.TEntGf6jyhDzpEI7ai2Ytzq66KOwmP2lEU4RQ8-CC5U')
 });
 
 app.get("/pfjV1A7h7Z-PfxFeD8QJ5mnSVoOySxitqyfs4ERQqPA", (req, res) => {
+    res.charset = 'utf-8'
     res.send('pfjV1A7h7Z-PfxFeD8QJ5mnSVoOySxitqyfs4ERQqPA.TEntGf6jyhDzpEI7ai2Ytzq66KOwmP2lEU4RQ8-CC5U')
 });
 
