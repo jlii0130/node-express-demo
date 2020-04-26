a690687ade26a911010afde2bc763505b037fa55
JJ <jlii0130@student.monash.edu>
2020-04-26 16:00:15 +1000

    reative design

---
+++ public/js/login.js
@@ -0,0 +1,8 @@
+function login() {
+    var username = document.getElementById('loginFormEmail').value
+    var password = document.getElementById('loginFormPassword').value
+
+    console.log(username)
+    console.log(password)
+
+}
